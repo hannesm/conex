f8c75c526b51ce9e6fc031b3922bc6cc0975d4b2
Hannes Mehnert <hannes@mehnert.org>
2016-09-20 20:06:22 +0100

    .

--- src/core.ml
+++ src/core.ml
@@ -6,8 +6,8 @@ module S = Set.Make(String)
 let pp_list pe ppf xs =
   Format.pp_print_string ppf "[" ;
   let rec p1 = function
-    | [] -> Format.pp_print_string ppf "]"
-    | [x] -> Format.fprintf ppf "%a]@." pe x
+    | [] -> Format.fprintf ppf "]@ "
+    | [x] -> Format.fprintf ppf "%a]@ " pe x
     | x::xs -> Format.fprintf ppf "%a,@ " pe x ; p1 xs
   in
   p1 xs
