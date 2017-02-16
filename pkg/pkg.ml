1ae8417200ae62110fb65f484b58fd6b057d22fe
Hannes Mehnert <hannes@mehnert.org>
2017-02-16 17:40:35 +0000

    require 4.03 (due to result), don't build analysis tools

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -11,8 +11,8 @@ let () =
   Ok [
     Pkg.mllib "src/conex.mllib" ;
     Pkg.mllib ~cond:nocrypto "src/nocrypto/conex-nocrypto.mllib" ;
-    Pkg.bin "analysis/maintainer" ;
-    Pkg.bin "analysis/opam_repo_stats" ;
+(*    Pkg.bin "analysis/maintainer" ;
+      Pkg.bin "analysis/opam_repo_stats" ; *)
     Pkg.bin "app/conex_author" ;
     Pkg.bin "app/conex_verify_openssl" ;
     Pkg.bin "app/conex_verify_nocrypto" ;
