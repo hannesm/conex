3af589d51a591db551a4adbd9dbaa1b10ac5b969
Hannes Mehnert <hannes@mehnert.org>
2017-02-16 23:16:00 +0000

    build: make opam-format a depopt

--- CHANGES.md
+++ CHANGES.md
@@ -1,3 +1,3 @@
-## ?? (unrelease)
+## 0.9.0 (2017-02-16)
 
 * initial release
--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -4,15 +4,18 @@
 open Topkg
 
 let nocrypto = Conf.with_pkg (* ~default:false *) "nocrypto"
+let format = Conf.with_pkg ~default:false "format"
 
 let () =
   Pkg.describe "conex" @@ fun c ->
-  let nocrypto = Conf.value c nocrypto in
+  let nocrypto = Conf.value c nocrypto
+  and format = Conf.value c format
+  in
   Ok [
     Pkg.mllib "src/conex.mllib" ;
     Pkg.mllib ~cond:nocrypto "src/nocrypto/conex-nocrypto.mllib" ;
-(*    Pkg.bin "analysis/maintainer" ;
-      Pkg.bin "analysis/opam_repo_stats" ; *)
+    Pkg.bin ~cond:format ~dst:"conex_maintainer" "analysis/maintainer" ;
+    Pkg.bin ~cond:format ~dst:"conex_repo_stats" "analysis/opam_repo_stats" ;
     Pkg.bin "app/conex_author" ;
     Pkg.bin "app/conex_verify_openssl" ;
     Pkg.bin "app/conex_verify_nocrypto" ;
